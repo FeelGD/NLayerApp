49e47da3bf6fb689e8c648e6812e2e460e9dd979
Ali İhsan YILDIZ <46348313+FeelGD@users.noreply.github.com>
2023-03-01 19:54:20 -0500

    Test VS git

--- NLayer.Core/Product.cs
+++ NLayer.Core/Product.cs
@@ -15,6 +15,7 @@ namespace NLayer.Core
         public int CategoryId { get; set; }
         public Category Category { get; set; }
         public ProductFeature ProductFeature { get; set; }
+        //nullable 
 
     }
 }
